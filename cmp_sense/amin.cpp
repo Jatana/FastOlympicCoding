d0fd47114b85d38c10ad10573f212497194538b6
Alex <alex.v.shekhovtsov@gmail.com>
2017-03-31 19:50:17 +0300

    Clean #4

--- cmp_sense/amin.cpp
+++ cmp_sense/amin.cpp
@@ -1,13 +1,230 @@
-// template<int a, int (int, int), double double , class Typical_Class>
-// long long l();
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include <queue>
+#include <ostream>
+#include <istream>
+#include <typeinfo>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+#include <limits>
+#include <fstream>
+#include <array>
+#include <list>
+#include <functional>
 
-class asd
-{
-public:
-    int a(int, int), b, c;
-    
+#define mt make_tuple
+#define x first
+#define y second
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define mp make_pair
+#define umap unordered_map
+#define uset unordered_set
+#define rt return 0;
+#define elif else if
+#define len(v) ((int)v.size())
+
+
+using namespace std;
+
+char string_in_buffer[(int)260];
+
+
+void fast_scan(int &x) { scanf("%d", &x); }
+void fast_scan(long long &x) { scanf("%lld", &x); }
+void fast_scan(unsigned long long &x) { scanf("%llu", &x); }
+void fast_scan(double &x) { scanf("%lf", &x); }
+void fast_scan(long double &x) { scanf("%Lf", &x); }
+void fast_scan(char &x) { 
+	scanf("%c", &x); 
+	if (x == '\n') {
+		fast_scan(x);
+	}
+}
+void fast_scan(string &x) {
+	scanf("%s", string_in_buffer);
+	x = string(string_in_buffer);
+}
+
+template<class TFirst, class TSecond>
+void fast_scan(pair<TFirst, TSecond> &p) {
+	fast_scan(p.first);
+	fast_scan(p.second);
+}
+
+template <class T>
+void fast_scan(vector<T> &v) {
+	for (auto &x : v) fast_scan(x);
+}
+
+void fast_print(const int &x) { printf("%d", x); }
+void fast_print(const long long &x) { printf("%lld", x); }
+void fast_print(const unsigned long long &x) { printf("%llu", x); }
+void fast_print(const double &x) { printf("%lf", x); }
+void fast_print(const long double &x) { printf("%Lf", x); }
+void fast_print(const char &x) { printf("%c", x); };
+void fast_print(const string &x) { printf("%s", x.c_str());}
+
+template<class TFirst, class TSecond>
+void fast_print(const pair<TFirst, TSecond> &p) {
+	fast_print(p.first);
+	fast_print(' ');
+	fast_print(p.second);
+}
+
+template <class T>
+void fast_print(const vector<T> &v) {
+	if (v.empty()) return;
+	fast_print(v[0]);
+	for (int i = 1; i < v.size(); i++) {
+		fast_print(" ");
+		fast_print(v[i]);
+	}
+}
+
+template <class T>
+void fast_print(const vector<vector<T>> &v) {
+	if (v.empty()) return;
+	fast_print(v[0]);
+	for (int i = 1; i < v.size(); i++) {
+		fast_print("\n");
+		fast_print(v[i]);
+	}
+}
+
+
+
+using namespace std;
+
+
+namespace smart_io {
+
+	string print_start = "";
+	string sep = " ";
+	bool first_print = false;
+
+	void precall_print() {
+		fast_print(print_start);
+		print_start = "\n";
+		first_print = true;
+	}
+}
+
+
+template <class T>
+ostream &operator,(ostream &os, const T &object) {
+
+	if (!smart_io::first_print) {
+		fast_print(smart_io::sep);
+	} else {
+		smart_io::first_print = false;
+	}
+	fast_print(object);
+	return os;
+}
+
+template <class T>
+istream &operator,(istream &is, T &object) {
+	fast_scan(object);
+	return is;
+}
+
+namespace typedefs {
+	typedef long long ll;
+	typedef unsigned long long ull;
+	typedef vector<int> vi;
+	typedef pair<int, int> pii;
+	typedef vector<vector<pair<int, int>>> vvpii;
+	typedef vector<vector<pair<bool, int>>> vvpbi;
+}
+
+using namespace typedefs;
+
+#define print    \
+smart_io::precall_print(); \
+cout,
+
+#define scan cin,
+
+struct Prev {
+	int last, n, h;
+
+	Prev(){}
+	Prev(int _last, int _n, int _h) {
+		last = _last;
+		n = _n;
+		h = _h;
+	}
 };
 
-int main() {
 
+int main(int argc, char *argv[]) {
+	int n, d, h;
+	scan n, d, h;
+	int max_n = n + 10;
+	int max_last = h + d + 10;
+	int max_h = h + 10;
+	vector<vector<vector<bool>>> dp(max_last, vector<vector<bool>>(max_n, vector<bool>(max_h, 0)));
+	vector<vector<vector<Prev>>> prev(max_last, vector<vector<Prev>>(max_n, vector<Prev>(max_h, Prev(-1, -1, -1))));
+	dp[d][d][1] = true;
+	for (int i = 2; i < max_h; i++) {
+		for (int j = 1; j < max_n; j++) {
+			for (int k = 1; k < max_last; k++) {
+				if (k - 1 >= 0 && j - k >= 0) {
+					if (dp[k - 1][j - k][i - 1]) {
+						dp[k][j][i] = true;
+						prev[k][j][i] = Prev(k - 1, j - k, i - 1);
+					}
+
+				}
+				if (k + 1 < max_last && j - k >= 0) {
+					if (dp[k + 1][j - k][i - 1]) {
+						dp[k][j][i] = true;
+						prev[k][j][i] = Prev(k + 1, j - k, i - 1);
+					}
+				}
+			}
+		}
+	}
+	int f_last = -1;
+	for (int last = 0; last < max_last; last++) {
+		if (dp[last][n][h]) {
+			f_last = last;
+			break;
+		}
+	}
+	if (f_last == -1) {
+		print 0;
+		return 0;
+	}
+	vector<int> rez;
+	Prev p = Prev(f_last, n, h);
+	while (!(p.last == d && p.n == d && p.h == 1)) {
+		rez.pb(p.last);
+		p = prev[p.last][p.n][p.h];
+	}
+	rez.pb(d);
+	reverse(rez.begin(), rez.end());
+	// assert(len(rez) == h);
+	// assert(rez[0] == d);
+	// int sm = 0;
+	// for (int i = 0; i < len(rez); i++) {
+	// 	sm += rez[i];
+	// }
+	// assert(sm == n);
+
+	for (auto x : rez) {
+		print x;
+	}
 }
