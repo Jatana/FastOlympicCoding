9105a03d16b6c4877cfb47cf8e5601f568ee286e
Shekhovtsov Alexander <alex.v.shekhovtsov@gmail.com>
2020-04-18 01:17:55 +0300

    Highlighting fixes

--- Highlight/test_interface.py
+++ Highlight/test_interface.py
@@ -6,5 +6,7 @@ base = path.dirname(__file__)
 def get_test_styles(view):
 	if view.settings().get('theme')	== 'Spacegray.sublime-theme':
 		return open(path.join(base, 'test_styles_spacegray.css')).read()
+	elif view.settings().get('theme') == 'Spacegray Light.sublime-theme':
+		return open(path.join(base, 'test_styles_spacegraylight.css')).read()
 	else:
 		return open(path.join(base, 'test_styles.css')).read()
